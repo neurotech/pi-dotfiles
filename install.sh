9ece2a9ca718456ef4faef0aa8201a9b72a65a3b
Tim Douglas <neurotech@gmail.com>
2017-02-01 20:55:30 +1100

    Added neofetch for motd

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-# apt
-echo "Updating apt and installing: git curl tmux fail2ban htop lynx wakeonlan watchdog"
-sudo apt-get update && sudo apt-get install git curl tmux fail2ban htop lynx wakeonlan watchdog
+# super
+sudo -v
+
+# essentials
+export ESSENTIALS="git curl tmux fail2ban htop lynx wakeonlan watchdog"
+echo "Updating apt and installing: $ESSENTIALS"
+sudo apt-get update && sudo apt-get install $ESSENTIALS
+
+# neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
+curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
+sudo apt-get update && sudo apt-get install neofetch
 
 # Link repo files to ~
-echo "Linking config files to ~"
-ln -sf ~/pi-dotfiles/.bashrc ~/.bashrc
-ln -sf ~/pi-dotfiles/.tmux.conf ~/.tmux.conf
+echo "Linking config files to $HOME"
+ln -sf $HOME/pi-dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/pi-dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/pi-dotfiles/neofetch/config $HOME/.config/neofetch/config
 
 # Refresh bash settings
 echo "Refreshing bash"
