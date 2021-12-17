bed0ef54f8f1ea3f58e645ac4bc67d41da5577eb
Tim Douglas <neurotech@gmail.com>
2021-12-18 09:05:19 +1100

    Updated install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo -v
 
 # essentials
-export ESSENTIALS="git curl tmux fail2ban htop lynx wakeonlan watchdog toilet"
+export ESSENTIALS="git curl tmux fail2ban htop lynx wakeonlan watchdog toilet neofetch"
 echo "Updating apt and installing: $ESSENTIALS"
 sudo apt-get update && sudo apt-get install $ESSENTIALS
 
@@ -15,7 +15,7 @@ sudo rm /etc/motd
 echo "Linking config files to $HOME"
 ln -sf $HOME/pi-dotfiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/pi-dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/pi-dotfiles/neofetch/config $HOME/.config/neofetch/config
+ln -sf $HOME/pi-dotfiles/neofetch/config $HOME/.config/neofetch/config.conf
 ln -sf $HOME/pi-dotfiles/neofetch/logo $HOME/.config/neofetch/logo
 echo "toilet -f smblock 'hovercastle' --filter crop:gay && echo '' && neofetch --colors 1 2 3 5 3 7" >> sudo /etc/profile
 
