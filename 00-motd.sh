5abd421db7962462534d2c60259330a982e771cc
Tim Douglas <neurotech@gmail.com>
2024-03-04 14:19:39 +1100

    MOTD fixes

--- 00-motd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-toilet -f smblock 'hovercastle' --filter crop:gay && echo '' && neofetch --colors 1 2 3 5 3 7
--- install.sh
+++ install.sh
@@ -10,14 +10,16 @@ sudo apt-get update && sudo apt-get install $ESSENTIALS
 
 # MOTD Preparation
 sudo rm /etc/motd
+sudo chmod -x /etc/update-motd.d/*
 
 # Link repo files
 echo "Linking config files..."
 ln -sf $PWD/.bashrc $HOME/.bashrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/neofetch/config $HOME/.config/neofetch/config.conf
-sudo ln -sf $PWD/00-motd.sh /etc/update-motd.d/00-motd.sh
-sudo chmod +x /etc/update-motd.d/00-motd.sh
+sudo ln -sf $PWD/motd.sh /etc/update-motd.d/motd.sh
+sudo chmod 755 /etc/update-motd.d/motd.sh
+
 
 # Refresh bash settings
 echo "Refreshing bash"
