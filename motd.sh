d70d80ef4dcc52366e42a96c7308f73b9e7f06b4
Tim Douglas <neurotech@gmail.com>
2024-03-04 14:29:44 +1100

    clear in motd

--- motd.sh
+++ motd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-toilet -f smblock 'hovercastle' --filter crop:gay && echo '' && neofetch --colors 1 2 3 5 3 7
+clear && toilet -f smblock 'hovercastle' --filter crop:gay && echo '' && neofetch --colors 1 2 3 5 3 7
